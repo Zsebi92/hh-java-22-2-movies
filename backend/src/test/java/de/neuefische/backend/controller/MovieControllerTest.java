8b2098a4cef839ba083bd9a17c613c2ad32bb4c2
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-09-30 22:10:42 +0200

    Add controller tests

---
+++ backend/src/test/java/de/neuefische/backend/controller/MovieControllerTest.java
@@ -0,0 +1,68 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.model.Movie;
+import de.neuefische.backend.respository.MovieRepo;
+import de.neuefische.backend.service.IdService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MovieControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    
+    @Autowired
+    private MovieRepo repo;
+
+    @MockBean
+    private IdService idService;
+
+
+    @DirtiesContext
+    @Test
+    void getAllMovies() throws Exception {
+        //GIVEN
+        Movie dummyMovie = new Movie("1", "Matrix", "1999", "www.post.com/image1.jpeg");
+        repo.add(dummyMovie);
+
+        //WHEN &THEN
+
+        mockMvc.perform(
+                get("/api/movie"))
+                .andExpect(status().is(200))
+                .andExpect(content().string("""
+                        [{"id":"1","title":"Matrix","releaseYear":"1999","poster":"www.post.com/image1.jpeg"}]"""));
+    }
+
+    @DirtiesContext
+    @Test
+    void addMovie() throws Exception {
+        //GIVEN
+        when(idService.generateId()).thenReturn("123");
+
+        //WHEN & THEN
+        mockMvc.perform(
+                        post("/api/movie")
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                                .content("""
+                                    {"title":"Matrix","releaseYear":"1999","poster":"www.post.com/image1.jpeg"}"""))
+                .andExpect(status().is(200))
+                .andExpect(content().string("""
+                        {"id":"123","title":"Matrix","releaseYear":"1999","poster":"www.post.com/image1.jpeg"}"""));
+    }
+
+}
