d1fcd8bd6007b330872bdcde8af1c3964d13ba0b
Zsebi92 <59623854+Zsebi92@users.noreply.github.com>
2022-10-10 09:14:36 +0200

    add deploytoheroku.yml

--- frontend/src/components/MovieCard.tsx
+++ frontend/src/components/MovieCard.tsx
@@ -9,7 +9,7 @@ export default function MovieCard(props:MovieCardProps){
 
     return(
         <div className={"movie-card"}>
-            <img src={props.movie.poster}/>
+            <img src={props.movie.poster} alt={props.movie.title}/>
             <p>{props.movie.title} ({props.movie.releaseYear})</p>
         </div>
     )
